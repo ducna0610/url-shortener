93a5911951fdd421b3800760715e5122810ef8de
ducna0610 <ducna0610@gmail.com>
2023-04-05 22:36:14 +0700

    init

---
+++ src/server.js
@@ -0,0 +1,52 @@
+
+import express from 'express';
+
+import expressLayouts from 'express-ejs-layouts';
+
+import session from 'express-session';
+
+import mongoose from 'mongoose';
+
+import cookieParser from 'cookie-parser';
+
+const app = express();
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import web from './routes/web.js';
+
+app.use(expressLayouts);
+app.set("view engine", ".ejs");
+app.set("views", "./src/views");
+app.set("layout extractScripts", true);
+app.set("layout extractStyles", true);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cookieParser())
+
+var sess = {
+  secret: 'keyboard cat',
+  cookie: {}
+}
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+
+app.use(session(sess));
+
+app.use("/assets", express.static("./src/public/assets/"));
+
+mongoose.connect(process.env.URL_DATABASE)
+  .then(() => console.log('Connected!'));
+
+web.initWebRoute(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`Sever is running!`);
+});
